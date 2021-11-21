38f765f31e8e2a5bd2941ec1b07a4aa49e55d3f3
roczhang <1587839905@qq.com>
2021-11-21 19:55:30 +0800

    req resp

--- src/main/java/com/roc/wiki/controller/DemoController.java
+++ src/main/java/com/roc/wiki/controller/DemoController.java
@@ -1,11 +1,15 @@
 package com.roc.wiki.controller;
 
 import com.roc.wiki.domain.Ebook;
+import com.roc.wiki.req.EbookReq;
 import com.roc.wiki.resp.CommonResp;
+import com.roc.wiki.resp.EbookResp;
 import com.roc.wiki.service.EbookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -31,6 +35,32 @@ public class DemoController {
         resp.setContent(ebookList);
         return resp;
     }
+
+    @GetMapping("/ebookLike")
+    public CommonResp ebookLikeList(String name) {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> ebookList = ebookService.listLike(name);
+        resp.setContent(ebookList);
+        return resp;
+    }
+
+    // 使用封装的请求
+    @GetMapping("/ebookLikeReq")
+    public CommonResp ebookLikeList(EbookReq req) {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> ebookList = ebookService.listLike(req.getName());
+        resp.setContent(ebookList);
+        return resp;
+    }
+
+    @GetMapping("/ebookLikeReqResp")
+    public CommonResp ebookLikeListResp(EbookReq req) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> ebookList = ebookService.listLikeResp(req.getName());
+        resp.setContent(ebookList);
+        return resp;
+    }
+
 
 
 }
--- src/main/java/com/roc/wiki/service/EbookService.java
+++ src/main/java/com/roc/wiki/service/EbookService.java
@@ -1,11 +1,16 @@
 package com.roc.wiki.service;
 
 import com.roc.wiki.domain.Ebook;
+import com.roc.wiki.domain.EbookExample;
 import com.roc.wiki.mapper.EbookMapper;
+import com.roc.wiki.req.EbookReq;
+import com.roc.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,4 +23,25 @@ public class EbookService {
         return ebookMapper.selectByExample(null);
     }
 
+    public List<Ebook> listLike(String name) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + name + "%");
+        return ebookMapper.selectByExample(ebookExample);
+    }
+
+    // 返回类。
+    public List<EbookResp> listLikeResp(String name) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + name + "%");
+        List<Ebook> ebooks = ebookMapper.selectByExample(ebookExample);
+        ArrayList<EbookResp> ebookResps = new ArrayList<>();
+        for (Ebook ebook : ebooks) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+            ebookResps.add(ebookResp);
+        }
+        return ebookResps;
+    }
 }
