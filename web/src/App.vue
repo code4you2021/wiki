f2cc42588cff5e477703373ee0610fad152bb423
roczhang <1587839905@qq.com>
2021-11-22 16:03:06 +0800

    add the footer component

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,20 +2,19 @@
   <a-layout>
     <theHeader/>
       <router-view/>
-    <a-layout-footer style="text-align: center">
-      Roc ©2021 Created by Roc
-    </a-layout-footer>
+    <theFooter/>
   </a-layout>
 </template>
 
 <script>
 // @ is an alias to /src
 import theHeader from '@/components/the-header'
+import theFooter from '@/components/the-footer'
 
 export default {
   name: 'App',
   components: {
-    theHeader
+    theHeader, theFooter
   }
 }
 </script>
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Roc ©2021 Created by Roc
+  </a-layout-footer>
+</template>
+
+<script>
+export default {
+  name: 'the-footer',
+}
+</script>
