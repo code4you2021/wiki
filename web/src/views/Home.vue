b70ebb9df032fedc001b9958fcbe7756ec0eaf86
roczhang <1587839905@qq.com>
2021-11-22 19:57:34 +0800

    database data display

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -46,29 +46,28 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+      <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
         <template #footer>
           <div>
-            <b>ant design vue</b>
+            <b>Roc design vue</b>
             footer part
           </div>
         </template>
         <template #renderItem="{ item }">
-          <a-list-item key="item.title">
-            <template #actions>
-          <span v-for="{ type, text } in actions" :key="type">
-            <component :is="type" style="margin-right: 8px" />
-            {{ text }}
-          </span>
+          <a-list-item key="item.name">
+            <template #actions> <!--这里是三个小图标-->
+              <span v-for="{ type, text } in actions" :key="type">
+                <component :is="type" style="margin-right: 8px" />
+                {{ text }}
+              </span>
             </template>
 
             <a-list-item-meta :description="item.description">
               <template #title>
-                <a :href="item.href">{{ item.title }}</a>
+                <a :href="item.href">{{ item.name }}</a>
               </template>
-              <template #avatar><a-avatar :src="item.avatar" /></template>
+              <template #avatar><a-avatar :src="item.cover" /></template>
             </a-list-item-meta>
-            {{ item.content }}
           </a-list-item>
         </template>
       </a-list>
